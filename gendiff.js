444d17cf3c9317d2a3ea1f42a4ecf3d6c53e63e6
ToshiKajitsu <toshikajitsu@gmail.com>
2022-04-30 12:47:12 +0400

    step 2

--- gendiff.js
+++ gendiff.js
@@ -5,7 +5,9 @@ const program = new Command();
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.8.0');
+  .version('0.8.0')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format')
 
 program.parse();
 
