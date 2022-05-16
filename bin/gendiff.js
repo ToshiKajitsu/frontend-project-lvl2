3fab41b461a51bba5f47ed429aa46bb694dfd109
ToshiKajitsu <toshikajitsu@gmail.com>
2022-05-16 20:36:45 +0400

    4 step GA add

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -13,7 +13,7 @@ program
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
     console.log(genDiff(filepath1, filepath2));
-});
+  });
 
 program.parse();
 
--- src/genDiff.js
+++ src/genDiff.js
@@ -2,42 +2,39 @@ import { readFileSync } from 'fs';
 import _ from 'lodash';
 import path from 'path';
 
- 
 const operators = { minus: '-', plus: '+' };
- 
- 
-const genDiff = (filepath1, filepath2) => {
-    let absoluteFile1 = '';
-    let absoluteFile2 = '';
-    if (filepath1.split('').includes('/')) {
-         absoluteFile1 = path.resolve(filepath1);
-         absoluteFile2 = path.resolve(filepath2);
-    } else {
-         absoluteFile1 = path.resolve('__fixtures__', filepath1);
-         absoluteFile2 = path.resolve('__fixtures__', filepath2);
-    }
-    absoluteFile1 = JSON.parse(readFileSync(absoluteFile1, 'utf8'));
-    absoluteFile2 = JSON.parse(readFileSync(absoluteFile2, 'utf8'));
-     const build = (absoluteFile1, absoluteFile2) => {
-          const keys = Object.keys({ ...absoluteFile1, ...absoluteFile2 });
-          const sortedKeys = _.sortBy(keys);
-          return sortedKeys.map((key) => {
-               const value1 = absoluteFile1[key];
-               const value2 = absoluteFile2[key];
-               if (!_.has(absoluteFile1, key)) {
-                    return `${operators.plus} ${key}: ${value2}`;
-               }
-               if (!_.has(absoluteFile2, key)) {
-                    return `${operators.minus} ${key}: ${value1}\n`;
-               }
-               if (!_.isEqual(value1, value2)) {
-                    return `${operators.minus} ${key}: ${value1} \n${operators.plus} ${key}: ${value2}\n`;
-               }
-                    return `${key} ${value1}\n`;
-          });  
-     };
-     return `{ \n${build(absoluteFile1, absoluteFile2).join('')}\n}`;
 
-}
- 
+const genDiff = (filepath1, filepath2) => {
+  let absoluteFile1 = '';
+  let absoluteFile2 = '';
+  if (filepath1.split('').includes('/')) {
+    absoluteFile1 = path.resolve(filepath1);
+    absoluteFile2 = path.resolve(filepath2);
+  } else {
+    absoluteFile1 = path.resolve('__fixtures__', filepath1);
+    absoluteFile2 = path.resolve('__fixtures__', filepath2);
+  }
+  absoluteFile1 = JSON.parse(readFileSync(absoluteFile1, 'utf8'));
+  absoluteFile2 = JSON.parse(readFileSync(absoluteFile2, 'utf8'));
+  const build = (file1, file2) => {
+    const keys = Object.keys({ ...file1, ...file2 });
+    const sortedKeys = _.sortBy(keys);
+    return sortedKeys.map((key) => {
+      const value1 = file1[key];
+      const value2 = file2[key];
+      if (!_.has(file1, key)) {
+        return `${operators.plus} ${key}: ${value2}`;
+      }
+      if (!_.has(file2, key)) {
+        return `${operators.minus} ${key}: ${value1}\n`;
+      }
+      if (!_.isEqual(value1, value2)) {
+        return `${operators.minus} ${key}: ${value1} \n${operators.plus} ${key}: ${value2}\n`;
+      }
+      return `${key} ${value1}\n`;
+    });
+  };
+  return `{\n${build(absoluteFile1, absoluteFile2).join('')}\n}`;
+};
+
 export default genDiff;
