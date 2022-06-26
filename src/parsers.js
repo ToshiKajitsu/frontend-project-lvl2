e610c2d769adb59da2f3212d8f20af808194eab1
ToshiKajitsu <toshikajitsu@gmail.com>
2022-06-26 14:57:53 +0400

    first edits

--- src/parsers.js
+++ src/parsers.js
@@ -1,12 +1,13 @@
 import yaml from 'js-yaml';
 
 export default (format, data) => {
-  switch (format) {
-    case '.json':
+  const gettFormat = format.split('.')[1];
+  switch (gettFormat) {
+    case 'json':
       return JSON.parse(data);
-    case '.yml':
+    case 'yml':
       return yaml.load(data);
-    case '.yaml':
+    case 'yaml':
       return yaml.load(data);
     default:
       throw new Error(`This format is not supported: ${format}`);
